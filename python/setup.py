a039ec11d27f895def33a0f21a2d3a19e361928a
rui <rui.zhou@wuage.com>
2018-03-05 11:22:07 +0800

    pypi

--- python/setup.py
+++ python/setup.py
@@ -9,9 +9,9 @@ from setuptools import setup
 打包的用的setup必须引入
 """
 
-VERSION = '1.0.1'
+VERSION = '1.1.0'
 
-with open('README.md') as f:
+with open('README.rst') as f:
     long_description = f.read()
 
 setup(
@@ -22,7 +22,7 @@ setup(
     classifiers=[],  # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
     keywords='python lunar solar converter',  # 关键字
     author='isee15',  # 用户名
-    author_email='zr0715@aliyun.com',  # 邮箱
+    author_email='isee15@outlook.com',  # 邮箱
     url='https://github.com/isee15/Lunar-Solar-Calendar-Converter',  # github上的地址,别的地址也可以
     license='MIT',  # 遵循的协议
     packages=['LunarSolarConverter'],  # 发布的包名
